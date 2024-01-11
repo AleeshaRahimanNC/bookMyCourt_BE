55d26fdae4ba399dbf908db31b2fd342d86a02aa
AleeshaRahimanNC <aleesharahiman@gmail.com>
2024-01-11 13:41:45 +0530

    3rd Commit: added tokens

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const dotEnv = require('dotenv').config();
 
 var authRouter = require('./routes/authRouter');
 var usersRouter = require('./routes/users');
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const USERS = require('../models/userSchema')
+const jwt = require('jsonwebtoken')
 
 const doSignUp = (req, res) =>{
 
@@ -20,10 +21,15 @@ const doSignUp = (req, res) =>{
 }
 
 const doLogin = async(req, res) =>{
+    console.log(req.body);
     const userDetails = await USERS.findOne({email:req.body.email})
 if(userDetails){
         if(userDetails.password===req.body.password){
-            res.status(200).json({message:'Login Successfull'})
+            let token=null
+            //console.log({token})
+            token=jwt.sign({userId:userDetails._id,name:userDetails.name,role:userDetails.role,email:userDetails.email},process.env.JWT_PASSWORD,{expiresIn:'2d'})
+            res.status(200).json(
+                {message:'Login Successfull',token})
         }
 }else{
     res.status(401).json({message:'Invalid Credentials'})
