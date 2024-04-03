81d3caf8d9b97e9b54dd20de58db5e6606234414
Aleesha Nishan <Aleesha@Aleeshas-MacBook-Pro.local>
2024-04-03 17:22:16 +0530

    14th commit: Added a new router in users and also created a getslotdata for booking purpose in userController folder

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,8 @@ const USERS = require("../models/userSchema");
 const COURTS = require("../models/courtSchema");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const COURT_SCHEDULES_MODEL = require("../models/courtSchedulesModel");
+const ObjectId = require("mongoose").Types.ObjectId;
 
 const doSignUp = (req, res) => {
   bcrypt.hash(
@@ -33,12 +35,12 @@ const doLogin = async (req, res) => {
     const { email, password } = req.body;
     const userDetails = await USERS.findOne({ email: email });
     if (userDetails) {
-    bcrypt.compare(password, userDetails.password, (err, result) => {
-      if (result) {
+      bcrypt.compare(password, userDetails.password, (err, result) => {
+        if (result) {
           // let token = null;
           //console.log({token})
-          userDetails.password=undefined
-          const options ={expiresIn: "2d"}
+          userDetails.password = undefined;
+          const options = { expiresIn: "2d" };
           const token = jwt.sign(
             {
               userId: userDetails._id,
@@ -48,24 +50,17 @@ const doLogin = async (req, res) => {
             },
             process.env.JWT_PASSWORD,
             options
-          )
-          res
-            .status(200)
-            .json({ userDetails, token })
+          );
+          res.status(200).json({ userDetails, token });
+        } else {
+          res.status(401).json({ message: "Invalid Credentials" });
         }
-       else {
-        res.status(401).json({ message: "Invalid Credentials" });
-      }
-    })
-    
-  } else {
-    res.status(401).json({ message: "Invalid Credentials" });
-  }
-} catch (error) {
-    
-  }
-}
-  
+      });
+    } else {
+      res.status(401).json({ message: "Invalid Credentials" });
+    }
+  } catch (error) {}
+};
 
 const getCourtsData = (req, res) => {
   COURTS.find()
@@ -87,4 +82,39 @@ const getCourtDatabyId = (req, res) => {
     });
 };
 
-module.exports = { doSignUp, doLogin, getCourtsData, getCourtDatabyId };
+const getslotsdata = (req, res) => {
+  let currentHour = 0;
+  currentDate = new Date(req.query.date);
+
+  if (new Date(new Date().setUTCHours(0, 0, 0, 0)) === currentDate) {
+    currentHour = new Date().getHours();
+  }
+
+  COURT_SCHEDULES_MODEL.aggregate([
+    {
+      $match: {
+        courtId: new ObjectId(req.query.courtId),
+        date: currentDate,
+        "slot.id": { $gte: currentHour }
+      }},
+      {
+        $project:{
+          _id:1,date:1,slot:1,cost:1,bookedBy:1
+        }
+      }
+    
+  ]).then((result) => {
+    console.log(result);
+    res.status(200).json(result);
+  });
+  try {
+  } catch (error) {}
+};
+
+module.exports = {
+  doSignUp,
+  doLogin,
+  getCourtsData,
+  getCourtDatabyId,
+  getslotsdata,
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 var express = require('express');
 const { userAuth } = require('../middleWares/authorization');
-const { getCourtsData, getCourtDatabyId } = require('../controllers/userController');
+const { getCourtsData, getCourtDatabyId, getslotsdata } = require('../controllers/userController');
 var  router = express.Router();
 
 router.get('/getCourtsData',userAuth, getCourtsData)
 router.get('/getCourtDatabyId',userAuth, getCourtDatabyId)
+router.get('/getslotsdata',userAuth, getslotsdata)
 
 module.exports = router;
