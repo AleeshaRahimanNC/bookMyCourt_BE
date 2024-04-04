44cdc094dd79d8306a98d0781d1ae73b452b4fc3
Aleesha Nishan <Aleesha@Aleeshas-MacBook-Pro.local>
2024-04-04 20:12:19 +0530

    16th commit: few changes made in authorization

--- middleWares/authorization.js
+++ middleWares/authorization.js
@@ -1,46 +1,47 @@
-const jwt =require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-const userAuth =(req,res,next)=>{ 
-    //Bearer uyfgoighbyx8uyblu7utdfyokjmvju7
-    try {
-        const token =req.headers['authorization'].split(' ')[1]
-        jwt.verify(token,process.env.JWT_PASSWORD,(err,decodedToken)=>{
-        if(decodedToken){
-            // console.log(decodedToken);
-            req.userId=decodedToken.userId
-            console.log(decodedToken)
-            next()
-        }else{
-            res.status(401).json('unauthorized user')
-        }
-        })
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err)
-    }
-   
-}
+const userAuth = (req, res, next) => {
+  //Bearer uyfgoighbyx8uyblu7utdfyokjmvju7
+  try {
+    const token = req.headers["authorization"].split(" ")[1];
+    jwt.verify(token, process.env.JWT_PASSWORD, (err, decodedToken) => {
+      if (decodedToken) {
+        // console.log(decodedToken);
+        req.userId = decodedToken.userId;
+        console.log(decodedToken);
+        next();
+      } else {
+        res.status(401).json("unauthorized user");
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+};
 
-const adminAuth =(req,res,next)=>{ 
-    //Bearer uyfgoighbyx8uyblu7utdfyokjmvju7
-    try {
-        
-        const token =req.headers['authorization'].split(' ')[1]
-        jwt.verify(token,process.env.JWT_PASSWORD,(err,decodedToken)=>{
-            // console.log(decodedToken.role);
-        if(decodedToken && decodedToken.role===1){
-            req.userId=decodedToken.userId
-            console.log(decodedToken)
-            next()
-        }else{
-            res.status(401).json('unauthorized user')
-        }
-        })
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err)
-    }
-   
-}
+const adminAuth = (req, res, next) => {
+  //Bearer uyfgoighbyx8uyblu7utdfyokjmvju7
+  try {
+    // console.log(req.headers);
+    const token = req.headers['authorization'].split(' ')[1];
+    jwt.verify(token, process.env.JWT_PASSWORD, (err, decodedToken) => {
+      // console.log(decodedToken);
+      if (decodedToken && decodedToken.role === 1) {
+        req.userId = decodedToken.userId;
+        // req.userId=decodedToken.userId;
+        console.log(decodedToken);
+        next();
+      } else {
+        res.status(401).json("unauthorized user");
+      }
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+};
 
-module.exports = {userAuth, adminAuth}
+
+
+module.exports = { userAuth, adminAuth };
